4db0b86e407740b1fa591683ba6601570f2a8ed6
pasanjg <44201430+pasanjg@users.noreply.github.com>
2020-08-10 05:49:48 +0530

    Labsheet 03

---
+++ Labsheet 03/StudentApp/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using StudentApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentApp.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        Student[] students = new Student[] {
+            new Student { Id = 1, Name = "Saman Kumara", School="Royal College", GuardianName="Sampath Kumara"},
+            new Student { Id = 2, Name = "John Doe", School="Kimgswood", GuardianName="James Doe"},
+            new Student { Id = 3, Name = "Angie Lee", School="Havard", GuardianName="Sunil Perera"},
+        };
+
+        [HttpGet]
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return students;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetStudent(int id)
+        {
+            var student = students.FirstOrDefault((s) => s.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public void myPostmethod()
+        {
+            Debug.WriteLine("My Post Method is called");
+        }
+    }
+}
---
+++ Labsheet 03/StudentApp/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String School { get; set; }
+        public String Grade { get; set; }
+        public String GuardianName { get; set; }
+    }
+}
